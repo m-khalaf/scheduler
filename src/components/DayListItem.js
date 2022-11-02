f413d8d4008555ec538f40a32d7cee32c600ec7d
User <user@Defaults-MacBook-Pro.local>
2022-11-02 20:47:21 +0300

    create DayList and create stories for it

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,15 +4,28 @@ import "components/DayListItem.scss";
 
 
 export default function DayListItem(props) {
+  const formatSpots = function (spots){
+    if (spots ===0){
+      return 'no spots';
+    }
+    if (spots===1){
+      return '1 spot';
+    }
+    if (spots>1){
+      return `${spots} spots`;
+    }
+  };
+
   const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
     "day-list__item--full" : !props.spots
   }
-  )
+  );
+
   return (
     <li onClick={() => props.setDay(props.name)} className={dayClass}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light"  >{props.spots} spots remaining</h3>
+      <h3 className="text--light"  >{formatSpots(props.spots)} remaining</h3>
     </li>
   );
 }
