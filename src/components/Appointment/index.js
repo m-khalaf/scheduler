e38358481e54908c803272e36211bdc2b56d170c
User <user@Defaults-MacBook-Pro.local>
2022-11-04 16:45:37 +0300

    Create appointment header and empty and add to stories

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,10 @@
+import "components/Appointment/styles.scss";
+import React from "react";
+
+export default function Appointment(props) {
+
+  return (
+    <article className="appointment"></article>
+
+  );
+};
