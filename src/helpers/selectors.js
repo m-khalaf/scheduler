22ab24ec4314506dd8211133be0bf02161fe476b
User <user@Defaults-MacBook-Pro.local>
2022-11-27 12:02:01 -0500

    Add getinterviwersforday function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -6,9 +6,17 @@ export function getAppointmentsForDay(state, day) {
   const appointmentsArray = Object.values(state.appointments);
   const result = appointmentsArray.filter(appointment => filteredDaysAppointmentsId.includes(appointment.id))
   return result;
-
 }
 
+export function getInterviewersForDay(state, day) {
+  if (state.days.length === 0) return []
+  const filteredDays = state.days.filter((dayObject => dayObject.name === day));
+  if (filteredDays.length === 0) return []
+  const filteredDaysInterviewersId = filteredDays[0].interviewers;
+  const interviewersArray = Object.values(state.interviewers);
+  const result = interviewersArray.filter(interviewer => filteredDaysInterviewersId.includes(interviewer.id))
+  return result;
+}
 
 export function getInterview(state, interview) {
   if (!interview) return null;
@@ -17,6 +25,5 @@ export function getInterview(state, interview) {
   const interviewer = interviewers.filter(interviewer => interviewer.id === interview.interviewer)
   result.interviewer = interviewer[0];
   return result;
-
 }
 
