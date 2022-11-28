278460958d0678a5dc5b505c3c3f10e0f86791fa
User <user@Defaults-MacBook-Pro.local>
2022-11-28 07:15:24 -0500

    Add test id attribute

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,26 +4,26 @@ import "components/DayListItem.scss";
 
 
 export default function DayListItem(props) {
-  const formatSpots = function (spots){
-    if (spots ===0){
+  const formatSpots = function (spots) {
+    if (spots === 0) {
       return 'no spots';
     }
-    if (spots===1){
+    if (spots === 1) {
       return '1 spot';
     }
-    if (spots>1){
+    if (spots > 1) {
       return `${spots} spots`;
     }
   };
 
   const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
-    "day-list__item--full" : !props.spots
+    "day-list__item--full": !props.spots
   }
   );
 
   return (
-    <li onClick={() => props.setDay(props.name)} className={dayClass}>
+    <li onClick={() => props.setDay(props.name)} className={dayClass} data-testid="day">
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light"  >{formatSpots(props.spots)} remaining</h3>
     </li>
